7ed57fdf5ff97ee600c05d88034d831e5797450e
Aditya Londhe <114740433+AdityaSLondhe@users.noreply.github.com>
2023-08-17 19:05:25 +0000

    Added Images

---
+++ src/Products.js
@@ -0,0 +1,11 @@
+import product1 from './assets/product/1.png'
+
+export const Products=[
+    {
+        id:1,
+        productName:'IPhone',
+        price:999.0,
+        productImage:product1
+    },
+
+]
---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Shop = () => {
+  return (
+    <div className='Shop'>
+        <div className="ShopTitle">
+            <h1>Shop</h1>
+        </div>
+        <div className="Products">
+            
+        </div>
+    </div>
+  )
+}
