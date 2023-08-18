5f9ec314d610dca9aad5afc8bee9975939e28c54
Aditya Londhe <114740433+AdityaSLondhe@users.noreply.github.com>
2023-08-18 19:13:00 +0000

    Created Product list and displayed it's content

--- src/Products.js
+++ src/Products.js
@@ -1,11 +1,59 @@
 import product1 from './assets/product/1.png'
-
-export const Products=[
+import product2 from './assets/product/2.png'
+import product3 from './assets/product/3.png'
+import product4 from './assets/product/4.png'
+import product5 from './assets/product/5.png'
+import product6 from './assets/product/6.webp'
+import product7 from './assets/product/7.webp'
+import product8 from './assets/product/8.webp'
+export const Products =[
     {
         id:1,
         productName:'IPhone',
         price:999.0,
         productImage:product1
+    },
+    {
+        id:2,
+        productName:'Macbook Pro',
+        price:1049.0,
+        productImage:product2
+    },
+    {
+        id:3,
+        productName:'Canon Camera',
+        price:532.0,
+        productImage:product3
+    },
+    {
+        id:4,
+        productName:'Jacket',
+        price:27.5,
+        productImage:product4
+    },
+    {
+        id:5,
+        productName:'LED Lights',
+        price:4.9,
+        productImage:product5
+    },
+    {
+        id:6,
+        productName:'Sweat-Shirt',
+        price:30.3,
+        productImage:product6
+    },
+    {
+        id:7,
+        productName:'Golf T-shirt',
+        price:15.7,
+        productImage:product7
+    },
+    {
+        id:8,
+        productName:'Dont trip Cap',
+        price:5.0,
+        productImage:product8
     },
 
 ]
---
+++ src/pages/shop/Product.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './shop.css'
+export const Product = (props) => {
+    const { id, productName, price, productImage} = props.data;
+
+  return (
+    <div className='Product'>
+        <img src={productImage} alt="image" />
+        <div className='description'>
+            <p><b>{productName}</b></p>
+            <p>${price}</p>
+        </div>
+    </div>
+  )
+}
