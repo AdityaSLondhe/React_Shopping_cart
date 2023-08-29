a615d24d034d9c4f739207ddcc0a7bb2349284a4
Aditya Londhe <114740433+AdityaSLondhe@users.noreply.github.com>
2023-08-29 18:49:14 +0000

    cart-item added

---
+++ src/pages/cart/cart-item.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Product } from '../shop/Product'
+
+export const CartItem = (props) => {
+  return (<div className='cartItem'>
+    <img src={productImage}/>
+    </div>)
+}
